120efc4f0eb999defe10ad90ed32cff85cff5f90
sametweb <sametmutevellioglu@gmail.com>
2020-12-29 15:59:45 -0800

    alt prop added

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="loading-overlay">
-      <img src="/logo.svg" width="50" height="50" className="loading-icon" />
+      <img src="/logo.svg" width="50" height="50" className="loading-icon" alt="Loading" />
     </div>
   );
 }
